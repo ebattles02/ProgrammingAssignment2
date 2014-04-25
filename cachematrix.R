dad973f996e1da45fdf2d18197dafc0b10762f55
Elliott Battles <elliott.battles@gmail.com>
2014-04-24 21:10:35 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal of this function is to create
+## a cached inverse of a matrix
 
 ## Write a short comment describing this function
 
