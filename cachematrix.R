02e29e6ea55a53377b4d50e28b3c5a04cab26b51
Elliott Battles <elliott.battles@gmail.com>
2014-04-24 21:29:29 -0500

    first update to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,27 @@
 ## The goal of this function is to create
 ## a cached inverse of a matrix
 
-## Write a short comment describing this function
+## this creates a special matrix which returns a 
+## list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedMatrix <- NULL
+    
+    set<- function(y){
+        x<<-y
+        cachedMatix <<- NULL
+        
+    }
+    
+    get <- function() x
+    
+    setMatrix <- function(solve) m
+    
+    getMatrix <- function() m
+    
+    list(set = set, get = get,
+         setMatrix = setMatrix
+         getMatix = getMatrix)
 }
 
 
